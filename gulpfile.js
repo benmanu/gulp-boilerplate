d4f0636430629ec59d8adffc42ffb96ad935ca4e
Ben Manu <bmanu10@gmail.com>
2015-07-02 00:32:47 +1200

    Updating and refining and adding icons

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,26 @@
 
 // requirements
 var gulp        = require('gulp');
-var gutil       = require('gulp-util');
-var size        = require('gulp-size');
-var changed     = require('gulp-changed');
 var sass        = require('gulp-sass');
 var minifyCSS   = require('gulp-minify-css');
-var concat      = require('gulp-concat');
-var uglify      = require('gulp-uglify');
-var rename      = require('gulp-rename');
 var prefix      = require('gulp-autoprefixer');
-var scsslint    = require('gulp-scss-lint');
 var browserify  = require('browserify');
-var jshint      = require('gulp-jshint');
 var stream      = require('vinyl-source-stream');
-var streamify   = require('gulp-streamify');
+var iconify     = require('gulp-iconify');
 
 // source paths
 var base = 'themes/default/'; // theme root
 
 var build = { // build folders (where will the development happen?)
 	scss: base + 'scss/',
-	js: base + 'js/' 
+	js: base + 'js/',
+    icon: base + 'icons/'
 }
 
 var dist = { // distribution folders (where should the output live?)
 	css: base + 'dist/css/',
-	js: base + 'dist/js/' 
+	js: base + 'dist/js/',
+    icon: base + 'dist/icons/'
 }
 
 // build paths
@@ -57,30 +51,22 @@ var paths = {
 			build.js + 'app.js',
 			build.js + 'utils/*.js'
 		]
-	}
+	},
+    icons: {
+        src: './' + build.icon + '*.svg',
+        output: './' + dist.icon,
+        pngOutput: './' + dist.icon + '/png/'
+    }
 }
 
-// CSS lint task
-gulp.task('css:lint', function() {
-
-	var src = build.scss + '*.scss';
-
-	// lint the files first
-	return gulp.src(src)
-    	.pipe(scsslint())
-    	.pipe(scsslint.failReporter());
-
-});
-
 // dev css processing (unminified)
-gulp.task('css', ['css:lint'], function() {
+gulp.task('css', function() {
 
 	var src = paths.css.src;
 	var dest = paths.css.dest;
 
 	// compile the css
     return gulp.src(src)
-    	.pipe(changed(dest))
         .pipe(sass({
         	sourceComments: 'map',
         	includePaths: [
@@ -88,11 +74,7 @@ gulp.task('css', ['css:lint'], function() {
         	]
         }))
         .pipe(prefix("last 1 version", "> 1%", "ie 8"))
-        .pipe(size({
-        	title: 'css'
-        }))
-        .pipe(gulp.dest(dest))
-        .on('error', gutil.log);
+        .pipe(gulp.dest(dest));
 
 });
 
@@ -105,11 +87,7 @@ gulp.task('css:prod', ['css'], function() {
     // compile the css
     return gulp.src(src)
         .pipe(minifyCSS())
-        .pipe(size({
-            title: 'min.css'
-        }))
-        .pipe(gulp.dest(dest))
-        .on('error', gutil.log);
+        .pipe(gulp.dest(dest));
 });
 
 // dev js processing (unminified)
@@ -122,11 +100,7 @@ gulp.task('js', function() {
     return browserify(src)
         .bundle()
         .pipe(stream('core.js'))
-        .pipe(streamify(size({
-            title: 'js'
-        })))
-        .pipe(gulp.dest(dest))
-        .on('error', gutil.log);
+        .pipe(gulp.dest(dest));
 
 });
 
@@ -141,12 +115,18 @@ gulp.task('js:prod', ['js'], function() {
         .transform({global: true}, 'uglifyify')
         .bundle()
         .pipe(stream('core.js'))
-        .pipe(streamify(size({
-            title: 'min.js'
-        })))
-        .pipe(gulp.dest(dest))
-        .on('error', gutil.log);
+        .pipe(gulp.dest(dest));
 
+});
+
+gulp.task('icon', function() {
+    iconify({
+        src: paths.icons.src,
+        pngOutput: paths.icons.pngOutput,
+        // scssOutput: build.scss,
+        cssOutput: paths.icons.output,
+        scssDisabled: true
+    });
 });
 
 gulp.task('watch', ['css:prod', 'js:prod'], function() {
--- themes/default/js/app.js
+++ themes/default/js/app.js
@@ -1,9 +1,3 @@
-
-window.$ = window.jQuery = require('jquery');
-
 var component = require('./components/component');
 component('something');
 component('something else');
-
-var carousel = require('./components/carousel');
-carousel('.carousel');
